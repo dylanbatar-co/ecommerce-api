6b266cb6190d5d8d04ad4554e4da287a6d6ebe94
dylanbatar-co <dbatista@condorlabs.io>
2021-01-11 22:57:53 -0500

    GraphQl Integration Query

--- src/graphql/resolvers/resolver.js
+++ src/graphql/resolvers/resolver.js
@@ -1,5 +1,15 @@
+const Product = require("../../entity/product.entity");
+
+const product = new Product();
+
 const rootResolver = {
-  hello: () => "hola que tal",
+  getAllProducts: async () => {
+    return await product.getAllProducts();
+  },
+  getProduct: async ({ id }) => {
+    const [result] = await product.getProduct(id);
+    return result;
+  },
 };
 
 module.exports = rootResolver;
--- src/graphql/schemas/schema.js
+++ src/graphql/schemas/schema.js
@@ -2,9 +2,22 @@ const { buildSchema } = require("graphql");
 
 const rootSchema = buildSchema(
   `
-		type Query{
-			hello:String
-		}
+	type Query {
+		getAllProducts: [Product]
+		getProduct(id:String): Product
+	}
+
+	type Product {
+		id:ID
+		name:String
+		categories:String
+		retail_price:Int
+		discounted_price:Int
+		description: String
+		raiting: String
+		brand: String
+		Image: [String]
+	}
 	`
 );
 
